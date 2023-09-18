e82a6106201e9891d833b15a0f62aeec466db9e0
Jhwan <ehgp77@naver.com>
2023-09-18 20:17:48 +0900

    study, mvvm, rxswift, rxcocoa

---
+++ RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 05/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MenuViewController: UIViewController {
+    let disposedBag = DisposeBag()
+    let viewModel = MenuViewModel()
+    let celliden = "MenuItemTableViewCell"
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel.menusOb
+            .bind(to: tableView.rx.items(cellIdentifier: celliden, cellType: MenuItemTableViewCell.self)) { (index, item, cell) in
+                cell.title.text = item.name
+                cell.price.text = "\(item.price)"
+                cell.count.text = "\(item.num)"
+                
+                cell.onChange = { [weak self] int in
+                    self?.viewModel.changeNum(item: item, increase: int)
+                }
+            }
+            .disposed(by: disposedBag)
+            
+        
+        viewModel.itemsCount
+            .map { "\($0)" }
+            .observe(on: MainScheduler.instance)
+            .bind(to: itemCountLabel.rx.text)
+            .disposed(by: disposedBag)
+        
+        viewModel.totalPrice
+            .map { $0.currencyKR() }
+            .observe(on: MainScheduler.instance)
+            .bind(to: totalPrice.rx.text )
+            .disposed(by: disposedBag)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let identifier = segue.identifier ?? ""
+        if identifier == "OrderViewController",
+            let orderVC = segue.destination as? OrderViewController {
+            // TODO: pass selected menus
+        }
+    }
+
+    func showAlert(_ title: String, _ message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertVC, animated: true, completion: nil)
+    }
+
+    // MARK: - InterfaceBuilder Links
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var itemCountLabel: UILabel!
+    @IBOutlet var totalPrice: UILabel!
+
+    @IBAction func onClear() {
+        viewModel.itemClear()
+    }
+
+    @IBAction func onOrder(_ sender: UIButton) {
+        // TODO: no selection
+        // showAlert("Order Fail", "No Orders")
+//        performSegue(withIdentifier: "OrderViewController", sender: nil)
+        
+        viewModel.menusOb.onNext([Menu(id: 0, name: "changed", price: 100, num: 2)])
+    }
+}
+
+//extension MenuViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 5
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
+//
+//        cell.title.text = "MENU \(indexPath.row)"
+//        cell.price.text = "\(indexPath.row * 100)"
+//        cell.count.text = "0"
+//
+//        return cell
+//    }
+//}
